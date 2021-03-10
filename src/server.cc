59ced0346d6f5d52f3a569595abdc1d3109e84a8
chanchann <www.769440972@qq.com>
2021-03-10 23:18:05 +0800

    crow

---
+++ src/server.cc
@@ -0,0 +1,19 @@
+#include "crow/crow_all.h"
+
+int main() {
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")
+    .methods("GET"_method)
+    ([](const crow::request& req){
+        std::cout << req.url_params << std::endl;
+        std::string cmd = req.url_params.get("cmd");
+        std::cout << cmd << std::endl;
+        // if(req.url_params.get("cmd") == "S") {
+        //     std::cout << "stop" << std::endl;
+        // }
+        return crow::response{"yes"};
+    });
+
+    app.port(9001).multithreaded().run();
+}
